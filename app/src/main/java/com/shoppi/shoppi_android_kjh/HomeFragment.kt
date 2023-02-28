22e7ac3691eab1ae8863ea490356f0cb3b3123f5
KJH <gh7052@gmail.com>
2023-02-28 16:09:52 +0900

    json 데이터를 문자열로 변환한것을 JSONObject, JSONArray로 변환해서 키 값으로 데이터 조회

--- app/src/main/java/com/shoppi/shoppi_android_kjh/HomeFragment.kt
+++ app/src/main/java/com/shoppi/shoppi_android_kjh/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment: Fragment() {
 
@@ -30,5 +31,14 @@ class HomeFragment: Fragment() {
         val assetLoader = AssetLoader()
         val homeData  = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")
+
+        if(!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+            val titleValue = Title(text, iconUrl)
+            titleValue.text
+        }
     }
 }
---
+++ app/src/main/java/com/shoppi/shoppi_android_kjh/Title.kt
@@ -0,0 +1,6 @@
+package com.shoppi.shoppi_android_kjh
+
+data class Title(
+    val text: String,
+    val iconUrl: String
+)
