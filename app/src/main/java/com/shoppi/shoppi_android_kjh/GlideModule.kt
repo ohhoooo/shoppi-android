83691fea58ad83611eb427eaea491dee31baf48e
KJH <gh7052@gmail.com>
2023-02-28 20:24:09 +0900

    Appbar의 TextView, ImageView 배치, NestedScrollView 배치, JSON 데이터 반영, Glide 라이브러리 활용

---
+++ app/src/main/java/com/shoppi/shoppi_android_kjh/GlideModule.kt
@@ -0,0 +1,7 @@
+package com.shoppi.shoppi_android_kjh
+
+import com.bumptech.glide.module.AppGlideModule
+
+@com.bumptech.glide.annotation.GlideModule
+class GlideModule : AppGlideModule() {
+}
--- app/src/main/java/com/shoppi/shoppi_android_kjh/HomeFragment.kt
+++ app/src/main/java/com/shoppi/shoppi_android_kjh/HomeFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import org.json.JSONObject
@@ -23,11 +25,8 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-        button.setOnClickListener {
-            findNavController().navigate(R.id.action_home_to_product_Detail)
-        }
-
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
         val assetLoader = AssetLoader()
         val homeData  = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")
@@ -37,8 +36,10 @@ class HomeFragment: Fragment() {
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
-            val titleValue = Title(text, iconUrl)
-            titleValue.text
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
         }
     }
 }
