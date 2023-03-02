eead655c74d3abf0a7fc95dc026fbe5479763125
KJH <gh7052@gmail.com>
2023-03-02 20:14:04 +0900

    json 포맷의 데이터를 객체로 변환하는 방법을 직접 구현 했다가 라이브러리 적용

--- app/src/main/java/com/shoppi/shoppi_android_kjh/Title.kt
+++ app/src/main/java/com/shoppi/shoppi_android_kjh/Title.kt
@@ -1,6 +1,8 @@
 package com.shoppi.shoppi_android_kjh
 
+import com.google.gson.annotations.SerializedName
+
 data class Title(
     val text: String,
-    val iconUrl: String
+    @SerializedName("icon_url") val iconUrl: String
 )
